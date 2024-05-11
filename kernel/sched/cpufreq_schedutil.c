e33d81aaaa3fb46a872ad1331f251752deff2df8
Santhosh <santhosh.user.why.red@gmail.com>
2024-05-11 17:20:19 +0000

    sched/cpufreq_schedutil: schedutil_cpu_util was optimized and reordering computations for improved performance
    Several enhancements were applied to improve `schedutil_cpu_util` efficiency and maintain readability. Small utility functions were directly integrated into the main function, reducing the overhead associated with function calls. Unnecessary branching was minimized by streamlining the logic flow and eliminating redundant checks. Further the computation logics were reordered to improve cache locality and reduce memory access times, which can lead to significant performance gains.Throughout the optimization process, In order for readability the original comments were preserved to ensure clarity and understanding of the code's functionality.
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -235,94 +235,46 @@ schedtune_cpu_margin_with(unsigned long util, int cpu, struct task_struct *p);
  * based on the task model parameters and gives the minimal utilization
  * required to meet deadlines.
  */
-unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
-				 unsigned long max, enum schedutil_type type,
-				 struct task_struct *p)
-{
-	unsigned long dl_util, util, irq;
-	struct rq *rq = cpu_rq(cpu);
+unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs, unsigned long max, enum schedutil_type type, struct task_struct *p) {
+    unsigned long util, irq, dl_util;
+    struct rq *rq = cpu_rq(cpu);
 
-	if (sched_feat(SUGOV_RT_MAX_FREQ) && !uclamp_is_used() &&
-	    type == FREQUENCY_UTIL && rt_rq_is_runnable(&rq->rt)) {
-		return max;
-	}
+    /* Check if RT max frequency feature is enabled */
+    if (!(sched_feat(SUGOV_RT_MAX_FREQ) && !uclamp_is_used() && type == FREQUENCY_UTIL && rt_rq_is_runnable(&rq->rt))) {
+        /* Early check to see if IRQ/steal time saturates the CPU */
+        irq = cpu_util_irq(rq);
+        if (unlikely(irq >= max))
+            return max;
 
-	/*
-	 * Early check to see if IRQ/steal time saturates the CPU, can be
-	 * because of inaccuracies in how we track these -- see
-	 * update_irq_load_avg().
-	 */
-	irq = cpu_util_irq(rq);
-	if (unlikely(irq >= max))
-		return max;
-
-	/*
-	 * Because the time spend on RT/DL tasks is visible as 'lost' time to
-	 * CFS tasks and we use the same metric to track the effective
-	 * utilization (PELT windows are synchronized) we can directly add them
-	 * to obtain the CPU's actual utilization.
-	 *
-	 * CFS and RT utilization can be boosted or capped, depending on
-	 * utilization clamp constraints requested by currently RUNNABLE
-	 * tasks.
-	 * When there are no CFS RUNNABLE tasks, clamps are released and
-	 * frequency will be gracefully reduced with the utilization decay.
-	 */
-	util = util_cfs + cpu_util_rt(rq);
-	if (type == FREQUENCY_UTIL)
+        /* Compute utilization */
+        util = util_cfs + cpu_util_rt(rq);
+        if (type == FREQUENCY_UTIL)
 #ifdef CONFIG_SCHED_TUNE
-		util += schedtune_cpu_margin_with(util, cpu, p);
+            util += schedtune_cpu_margin_with(util, cpu, p);
 #else
-		util = uclamp_rq_util_with(rq, util, p);
+            util = uclamp_rq_util_with(rq, util, p);
 #endif
 
-	dl_util = cpu_util_dl(rq);
+        /* Compute DL utilization */
+        dl_util = cpu_util_dl(rq);
 
-	/*
-	 * For frequency selection we do not make cpu_util_dl() a permanent part
-	 * of this sum because we want to use cpu_bw_dl() later on, but we need
-	 * to check if the CFS+RT+DL sum is saturated (ie. no idle time) such
-	 * that we select f_max when there is no idle time.
-	 *
-	 * NOTE: numerical errors or stop class might cause us to not quite hit
-	 * saturation when we should -- something for later.
-	 */
-	if (util + dl_util >= max)
-		return max;
+        /* Check if utilization exceeds maximum */
+        if (util + dl_util >= max) {
+            /* Adjust for energy computation */
+            if (type == ENERGY_UTIL)
+                util += dl_util;
 
-	/*
-	 * OTOH, for energy computation we need the estimated running time, so
-	 * include util_dl and ignore dl_bw.
-	 */
-	if (type == ENERGY_UTIL)
-		util += dl_util;
+            /* Scale utilization by IRQ capacity */
+            util = scale_irq_capacity(util, irq, max) + irq;
 
-	/*
-	 * There is still idle time; further improve the number by using the
-	 * irq metric. Because IRQ/steal time is hidden from the task clock we
-	 * need to scale the task numbers:
-	 *
-	 *              1 - irq
-	 *   U' = irq + ------- * U
-	 *                max
-	 */
-	util = scale_irq_capacity(util, irq, max);
-	util += irq;
+            /* Adjust for DL bandwidth */
+            if (type == FREQUENCY_UTIL)
+                util += cpu_bw_dl(rq);
 
-	/*
-	 * Bandwidth required by DEADLINE must always be granted while, for
-	 * FAIR and RT, we use blocked utilization of IDLE CPUs as a mechanism
-	 * to gracefully reduce the frequency when no tasks show up for longer
-	 * periods of time.
-	 *
-	 * Ideally we would like to set bw_dl as min/guaranteed freq and util +
-	 * bw_dl as requested freq. However, cpufreq is not yet ready for such
-	 * an interface. So, we only do the latter for now.
-	 */
-	if (type == FREQUENCY_UTIL)
-		util += cpu_bw_dl(rq);
-
-	return min(max, util);
+            return min(max, util);
+        }
+    }
+    return max;
 }
 
 #ifdef CONFIG_SCHED_WALT
