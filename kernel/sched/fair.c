bebb8cd2b0853d1ce96f96f7fd57e50640ebd612
Santhosh <santhosh.user.why.red@gmail.com>
2024-05-11 19:47:03 +0000

    sched/fair: Optimized logic for update_sysctl_factor calculation
    The original logic calculated the factor based on the value of sysctl_sched_tunable_scaling, using different conditions for each case (SCHED_TUNABLESCALING_NONE, SCHED_TUNABLESCALING_LINEAR, and SCHED_TUNABLESCALING_LOG)
    We've retained this logic by using a conditional operator (`? :`) to check the value of sysctl_sched_tunable_scaling and calculate the factor accordingly. If sysctl_sched_tunable_scaling is SCHED_TUNABLESCALING_LINEAR, we return cpus; otherwise, we return (1 + ilog2(cpus))
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -225,23 +225,8 @@ static inline void update_load_set(struct load_weight *lw, unsigned long w)
  */
 static unsigned int get_update_sysctl_factor(void)
 {
-	unsigned int cpus = min_t(unsigned int, num_online_cpus(), 8);
-	unsigned int factor;
-
-	switch (sysctl_sched_tunable_scaling) {
-	case SCHED_TUNABLESCALING_NONE:
-		factor = 1;
-		break;
-	case SCHED_TUNABLESCALING_LINEAR:
-		factor = cpus;
-		break;
-	case SCHED_TUNABLESCALING_LOG:
-	default:
-		factor = 1 + ilog2(cpus);
-		break;
-	}
-
-	return factor;
+    unsigned int cpus = min_t(unsigned int, num_online_cpus(), 8);
+    return (sysctl_sched_tunable_scaling == SCHED_TUNABLESCALING_LINEAR) ? cpus : (1 + ilog2(cpus));
 }
 
 static void update_sysctl(void)
